ff15ded2e1a27cc00728b4c01540be1fe5c867ec
campbelllm <campbelllm@gmail.com>
2023-01-30 13:00:32 -0800

    tetrominos created

---
+++ app.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContectLoaded", () => {
+    const grid = document.querySelector('.grid')
+    let square = Array.from(document.querySelector('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
+
+    const lTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ]
+
+    const zTetromino = [
+        [width * 2, width * 2 + 1, width + 1, width + 2],
+        [0, width, width + 1, width * 2 + 1],
+        [width * 2, width * 2 + 1, width + 1, width + 2],
+        [0, width, width + 1, width * 2 + 1],
+    ]
+
+    const tTetromino = [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width * 2 + 1, width + 2],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
+    ]
+
+    const oTetromino = [
+        [0, width, 1, width + 1],
+        [0, width, 1, width + 1],
+        [0, width, 1, width + 1],
+        [0, width, 1, width + 1],
+    ]
+
+    const iTetromino = [
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3,],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+    ]
+
+    const
+    const theTetrominos = [lTetromino]
+    let currentPosition = 4
+    let current = theTetrominos[0][0]
+})
