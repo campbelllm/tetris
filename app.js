e9f0b625533e97b7fb602c9472cd98fab83600e4
campbelllm <campbelllm@gmail.com>
2023-01-31 13:55:11 -0800

    move tetrominos left right and down, creating boarders to stop at edges

--- app.js
+++ app.js
@@ -68,6 +68,20 @@ document.addEventListener("DOMContentLoaded", () => {
     /// move down the tetromino
     timerId = setInterval(moveDown, 1000)
 
+    //function for keyCodes (number associated with each key)
+    function control(e) {
+        if(e.keyCode === 37){
+            moveLeft()
+        }else if (e.keyCode === 38) {
+            //rotatoe
+        }else if (e.keyCode === 39){
+            moveRight()
+        }else if (e.keyCode === 40) {
+            moveDown()
+        }
+    }
+    document.addEventListener('keyup', control)
+ 
     function moveDown() {
         undraw()
         currentPosition += width
@@ -90,9 +104,34 @@ document.addEventListener("DOMContentLoaded", () => {
             currentPosition = 4
             draw()
         }
-
     }
 
+    //set outside boundaries for game, stop from overlapping other tetrominos
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
 
+        if (!isAtLeftEdge) currentPosition -= 1
+
+        if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition +=1
+        }
+
+        draw()
+    }
+
+    function moveRight() {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+
+
+        if(!isAtRightEdge) currentPosition+=1
+
+        if(current.some(index=> squares[currentPosition + index].classList.contains('taken'))){
+            currentPosition -=1
+        }
+        draw()
+    }
+    
 
 })
