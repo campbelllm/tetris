fee2bd0867ae26529bc6073e101ac74c1ed25e7b
campbelllm <campbelllm@gmail.com>
2023-02-01 16:06:55 -0800

    line is full remove and add points to score

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const width = 10
     let nextRandom = 0
     let timerId
-
+    let score = 0
     // creating the Tetrominos(shapes in tetris game)
     const lTetromino = [
         [1, width + 1, width * 2 + 1, 2],
@@ -105,6 +105,7 @@ document.addEventListener("DOMContentLoaded", () => {
             currentPosition = 4
             draw()
             displayShape()
+            addScore()
         }
     }
 
@@ -188,6 +189,23 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
     )
+    function addScore() {
+        for (let i = 0; i < 199; i += width) {
+            const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9]
+
+            if (row.every(index => squares[index].classList.contains('taken'))) {
+                score += 10
+                scoreDisplay.innerHTML = score
+                row.forEach(index => {
+                    squares[index].classList.remove('taken')
+                    squares[index].classList.remove('tetromino')
+                })
+                const squaresRemoved = squares.splice(i, width)
+                squares = squaresRemoved.concat(squares)
+                squares.forEach(cell => grid.appendChild(cell))
+            }
+        }
+    }
 
 
 
